d74d5d2d4c6ec9c802c6b08b45026c5b429a5572
Garima Mishra <garima26062005@gmail.com>
2025-05-27 23:50:22 +0530

    Create a.bat

---
+++ a.bat
@@ -0,0 +1,22 @@
+@echo off
+cd /d "C:\Path\To\Your\Project"  :: Replace with your local folder path
+
+echo Initializing Git repository...
+git init
+
+echo Adding all files...
+git add .
+
+echo Creating initial commit...
+git commit -m "first commit"
+
+echo Setting branch to main...
+git branch -M main
+
+echo Adding remote origin...
+git remote add origin https://github.com/garimamishra4/a.bat.git
+echo Pushing to GitHub...
+git push -u origin main
+
+echo Done!
+pause
